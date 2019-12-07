691a898aa793a5ac4e55a076d9eb6e1fdc84ba64
Kirstine Berg Jørgensen <kbjo@itu.dk>
2019-12-07 09:18:38 +0100

    Add files via upload
    Det her er mit bedste shot at MVC (Model - view - Controller)
    Model er den eneste klasse der må snakke med alle vores "normale" klasser.
    FXML filerne må KUN snakke med diverse controllere. 
    Controllerne snakker så både med modellen og FXML filerne.
    
    Tag jer noget tid til at kigge rundt hvordan jeg har gjort det inden i laver om / tilføjer :P

---
+++ GoatSample/GoatView.java
@@ -0,0 +1,4 @@
+package GoatSample;
+
+public class GoatView {
+}
---
+++ GoatSample/Model.java
@@ -0,0 +1,57 @@
+package GoatSample;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Model {
+
+    User user;
+    List<User> users;
+    List<Movie> movies;
+    List<Series> series;
+    private static Model model;
+    int chosenUser;
+    String userName;
+    private Model() {
+        users = new ArrayList<>();
+    }
+
+    public static Model getInstance() {
+        if (model == null) {
+            model = new Model();
+        }
+        return model;
+    }
+
+    public List<Movie> getMovies() throws IOException {
+        MovieReader mr = new MovieReader();
+        return mr.readMovies("film.txt");
+    }
+
+    public List<Series> getSeries() throws IOException {
+        SeriesReader sr = new SeriesReader();
+        return sr.readSeries("serier.txt");
+    }
+
+    public void addUser(int chosenUser) {
+        this.chosenUser = chosenUser;
+    }
+
+    public void addUserName(String name) {
+        userName = name;
+    }
+
+    public int getUser() {
+        return chosenUser;
+    }
+
+    public String getUserName() {
+            return userName;
+        }
+    }
+
+
+
+
+
