948ec584fedbd1e56a41f60e8f66dc0f5cb2bd23
tjoms tjomson <thhk@itu.dk>
2019-12-10 11:51:13 +0100

    Main fix

---
+++ src/sample/Main.java
@@ -0,0 +1,24 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("GOAT.fxml"));
+        stage.setTitle("Goat");
+        stage.setScene(new Scene(root, 400, 200));
+        stage.show();
+    }
+
+
+}
