09e58c181cac1accae5ebce1e7e0162c2d32a184
Kirstine Berg Jørgensen <kbjo@itu.dk>
2019-12-11 13:54:05 +0100

    hejsa der er billeder,

--- src/sample/goatdemo.java
+++ src/sample/goatdemo.java
@@ -1,10 +1,15 @@
 package sample;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 
 public class goatdemo extends Application {
@@ -23,7 +28,13 @@ public class goatdemo extends Application {
 
             Parent root = FXMLLoader.load(getClass().getResource("StartScreen.fxml"));
             stage.setTitle("Goat");
-            stage.setScene(new Scene(root, 400, 200));
+            Scene scene = new Scene(root, 400, 300);
+            stage.setScene(scene);
+
+            FileInputStream f = new FileInputStream("GoatLogo.png");
+            Image image = new Image(f);
+            ImageView im = new ImageView(image);
+
             stage.show();
         }
 
