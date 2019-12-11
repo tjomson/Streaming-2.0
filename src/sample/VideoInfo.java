7495ce2b776c289da33423d7ffba403c78841060
Thomas Hoffmann Kilbak <thhk@itu.dk>
2019-12-11 17:24:17 +0100

    VideoInfo gør seje ting

--- src/sample/VideoInfo.java
+++ src/sample/VideoInfo.java
@@ -4,60 +4,24 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.media.Media;
 import javafx.stage.Stage;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class VideoInfo extends Application {
-
-    public void lmao(Video video) throws FileNotFoundException {
-        HBox  imageInfoBox = new HBox();
-        VBox infoBox = new VBox();
-
-        FileInputStream f;
-        if (video instanceof Movie) {
-            f = new FileInputStream("Billeder/" + video.getTitle() + ".jpg");
-        } else {
-            f = new FileInputStream("Serier - billeder/" + video.getTitle() + ".jpg");
-        }
-        Image image = new Image(f);
-        ImageView imageView = new ImageView(image);
-
-        Label yearLabel;
-        if (video instanceof Series) {
-            Series series1 = (Series) video;
-            String endYearString;
-            if (series1.getEndYear() == 0) {
-                endYearString = "";
-            } else {
-                endYearString = series1.getEndYear() + "";
-            }
-            yearLabel = new Label(video.getYear() + "-" + endYearString);
-        } else {
-            yearLabel = new Label("" + video.getYear());
-        }
-
-
-        HBox genreField = new HBox();
-        for (String genre : video.getGenres()) {
-            genreField.getChildren().add(new Label(genre + " "));
-        }
-
-        infoBox.getChildren().addAll(new Label(video.getTitle()),yearLabel,genreField,new Label(video.getRating() + ""));
-
-
-        imageInfoBox.getChildren().addAll(imageView,infoBox);
-
-    }
-
 
     public static void main(String[] args) {
         launch(args);
@@ -68,11 +32,19 @@ public class VideoInfo extends Application {
 
     public void start(Stage stage) throws IOException {
         String[] xd = {"lmao","bruhhh","xddddddddd"};
-        Video video = new Movie("Fargo (the movie)", 1337,xd,2.3);
+        Map<Integer,Integer> mappp = new HashMap<>();
+        for(int i = 1; i <= 7; i++){
+            mappp.put(i,i+10);
+        }
+
+        Video video = new Series("Fargo", 1337,xd,2.3,1450,mappp);
 
 
         HBox  imageInfoBox = new HBox();
+        imageInfoBox.setSpacing(20);
         VBox infoBox = new VBox();
+        VBox window = new VBox();
+        infoBox.setSpacing(20);
 
         FileInputStream f;
         if (video instanceof Movie) {
@@ -92,25 +64,51 @@ public class VideoInfo extends Application {
             } else {
                 endYearString = series1.getEndYear() + "";
             }
-            yearLabel = new Label(video.getYear() + "-" + endYearString);
+            yearLabel = new Label("Year: " + video.getYear() + "-" + endYearString);
         } else {
-            yearLabel = new Label("" + video.getYear());
+            yearLabel = new Label("Year: " + video.getYear());
         }
 
 
         HBox genreField = new HBox();
+        genreField.getChildren().add(new Label("Genres: "));
         for (String genre : video.getGenres()) {
             genreField.getChildren().add(new Label(genre + " "));
         }
 
-        infoBox.getChildren().addAll(new Label(video.getTitle()),yearLabel,genreField,new Label(video.getRating() + ""));
+        infoBox.getChildren().addAll(new Label("Title: " + video.getTitle()),yearLabel,genreField,new Label("Rating: " + video.getRating() + ""));
 
 
         imageInfoBox.getChildren().addAll(imageView,infoBox);
 
-        
+        window.getChildren().add(imageInfoBox);
+
+
+        if(video instanceof Series){
+            Series series = (Series) video;
+            ScrollPane scrollPane = new ScrollPane();
+            VBox seasonsBox = new VBox();
+            scrollPane.setContent(seasonsBox);
+            scrollPane.setFitToWidth(true);
+            infoBox.getChildren().add(new Label("Number of seasons: " + series.getSeasons().size()));
+            window.getChildren().add(scrollPane);
+
+
+            for(Map.Entry entry : series.getSeasons().entrySet()){
+
+                FlowPane episodesFlowPane = new FlowPane();
+                int numberOfEpisodes = (int) entry.getValue();
+                for(int i = 1; i <= numberOfEpisodes; i++){
+                    episodesFlowPane.getChildren().add(new Button("Episode " + i));
+                }
+                seasonsBox.getChildren().addAll(new Label("Season " + entry.getKey()),episodesFlowPane);
+            }
+
+        }
+
+
         stage.setTitle("Goat");
-        stage.setScene(new Scene(imageInfoBox, 400, 200));
+        stage.setScene(new Scene(window, 500, 500));
         stage.show();
     }
 }
