13870fb66a72bd3d8a33087d802e760af41d4781
Ida Pallesen Suhr <idsu@itu.dk>
2019-12-13 12:43:49 +0100

    User til tekstfil

--- src/sample/ControllerChooseUser.java
+++ src/sample/ControllerChooseUser.java
@@ -4,18 +4,20 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextInputDialog;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Optional;
 
 
 public class ControllerChooseUser {
 
     @FXML
-    Button User1Button;
+    Button user1Button;
     @FXML
-    Button User2Button;
+    Button user2Button;
     @FXML
-    Button User3Button;
+    Button user3Button;
 
     Model model;
 
@@ -25,7 +27,7 @@ public class ControllerChooseUser {
         model = Model.getInstance();
         model.addUserNumber(1);
 
-        if (!User1Button.getText().equals("New User")) {
+        if (!user1Button.getText().equals("New User")) {
             openStartSceneMethod();
             model.getCurrentStage().close();
         } else {
@@ -38,7 +40,7 @@ public class ControllerChooseUser {
         model = Model.getInstance();
         model.addUserNumber(2);
 
-        if (!User2Button.getText().equals("New User")) {
+        if (!user2Button.getText().equals("New User")) {
             openStartSceneMethod();
             model.getCurrentStage().close();
         } else {
@@ -51,7 +53,7 @@ public class ControllerChooseUser {
         model = Model.getInstance();
         model.addUserNumber(3);
 
-        if (!User3Button.getText().equals("New User")) {
+        if (!user3Button.getText().equals("New User")) {
             openStartSceneMethod();
             model.getCurrentStage().close();
         } else {
@@ -62,6 +64,30 @@ public class ControllerChooseUser {
     public void openStartSceneMethod() throws IOException, noSuchVideoException {
         ControllerVidSelection c = new ControllerVidSelection();
         c.openStartScene();
+    }
+
+    public void writeNewUser1() {
+        try (BufferedWriter bw = new BufferedWriter(new PrintWriter("user1.txt"))) {
+            bw.write(user1Button.getText());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void writeNewUser2(){
+        try (BufferedWriter bw = new BufferedWriter(new PrintWriter("user2.txt"))) {
+            bw.write(user2Button.getText());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void writeNewUser3(){
+        try (BufferedWriter bw = new BufferedWriter(new PrintWriter("user3.txt"))) {
+            bw.write(user3Button.getText());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void openChangeUserWindow() throws IOException {
@@ -76,15 +102,15 @@ public class ControllerChooseUser {
         if (result.isPresent()) {
             if (model.getUserNumber() == 1) {
                 model.addUserName(result.get());
-                User1Button.setText(result.get());
+                user1Button.setText(result.get());
             }
             if (model.getUserNumber() == 2) {
                 model.addUserName(result.get());
-                User2Button.setText(result.get());
+                user2Button.setText(result.get());
             }
             if (model.getUserNumber() == 3) {
                 model.addUserName(result.get());
-                User3Button.setText(result.get());
+                user3Button.setText(result.get());
 
             }
         }
