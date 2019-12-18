35b20a4f50cc0944e04ec81b24c2b0294aed2dba
tjoms tjomson <thhk@itu.dk>
2019-12-18 12:50:02 +0100

    Update TooOldVideoException.java

--- src/sample/TooOldVideoException.java
+++ src/sample/TooOldVideoException.java
@@ -1,10 +1,10 @@
 package sample;
 
-//Unchecked exception som
+//Unchecked exception som kastes når en video er for gammel.
 public class TooOldVideoException extends RuntimeException {
 
     public TooOldVideoException(int year){
-        super("*** Videoer var ikke opfundet dengang!!! Dette år er ikke validt: " + year);
+        super("*** Videoer var ikke opfundet dengang!!! Dette år er ikke tilladt: " + year);
 
     }
 }
